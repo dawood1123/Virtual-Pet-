0db3dbe931df3d427e2d818b18c8cf26c5c90f0c
dawood1123 <davidkhan1226@gmail.com>
2024-10-14 20:27:23 +0500

    JS added

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-function get_value(){ 
+var pet_types;
 
-    
+
+var type = document.getElementById('pet_types'); 
+    type.addEventListener('change', function() {
+         pet_types = type.value; 
+        console.log(pet_types)
+    });
+function get_value(){ 
     var name = document.getElementById('pet_name'); 
     var pet_name = name.value; 
-
-    var type = document.getElementById('pet_type'); 
-    type.addEventListener('change', function() {
-        var pet_type = type.value;  
-    });
+    console.log(pet_name)
+    
 
     var age = document.getElementById('pet_age'); 
     var pet_age = age.value; 
@@ -18,7 +21,7 @@ function get_value(){
     var hunger = document.getElementById('pet_hungry'); 
     var pet_hungry = hunger.value; 
 
-    if((pet_name.trim === "") || (pet_type === "-") || (pet_age=== "0") ||(pet_happy=== "0") ||(pet_hungry=== "0") )
+    if((pet_name.trim === "") || (pet_types === "-") || (pet_age=== "0") ||(pet_happy=== "0") ||(pet_hungry=== "0") )
     {
         alert("Plz Fill all fields")
     }
@@ -37,6 +40,7 @@ function get_value(){
             line.textContent=(`${pet_name} had fun playing! Happiness level is now ${pet_happy}.`);
     
         }
+
     
         function agePet(){ 
             pet_age++; 
@@ -48,13 +52,15 @@ function get_value(){
         var div_content =document.getElementById("left")
         div_content.innerHTML = "";
     
-        if(pet_type = "Cat"){ 
+        if(pet_types == "Cat"){ 
             document.getElementById("image").src = "icons8-cat-100.png"
+            console.log(document.getElementById("image").src)
         }
-        else if (pet_type ="Dog"){
+        else if (pet_types=="Dog"){
             document.getElementById("image").src = "icons8-dog-100.png"
+            console.log(document.getElementById("image").src)
         }
-        else if (pet_type ="Rabbit"){
+        else if (pet_types =="Rabbit"){
             document.getElementById("image").src = "icons8-rabbit-100.png"
         }
         else{ 
@@ -88,21 +94,12 @@ function get_value(){
         div_content.style.display = "flex"; 
         div_content.style.flexDirection = "row"
         div_content.style.gap = "20px"
-        div_content.style.width = "40%"
+        div_content.style.width = "90%"
+
     
         // Append the buttons to the div
         div_content.appendChild(feedButton);
         div_content.appendChild(playButton);
         div_content.appendChild(agePetButton);
-
     }
-
-
-}
-
-
-
-
-
-
- 
+};
